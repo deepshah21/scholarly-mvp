bc2cb0a8eb706027909c34894b7aee3cc3f2b396
Meet Patel <meetpatel9814@gmail.com>
2019-05-27 21:37:02 +0530

    # Adding functionality for attendance in student page from admin side. # Select Semester -> render attendance for that semester(ONLY UI)

--- src/components/pages/student/Attendace.js
+++ src/components/pages/student/Attendace.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React,{Component} from "react";
+import { Dropdown, Table } from 'semantic-ui-react';
+import data from '../../../data.json';
 
 
-const Attendance = () => {
-    return(
-        <div>
-            Attendance
-            </div>
-    )
+class Attendance extends Component {
+  state = {
+    slectedSemester : 2,
+    attendanceData : [],
+    semesterOptions : []
+  }
+  componentDidMount() {
+    let semesterOptions = data.semesterList.map(
+      semester => ({
+      key: semester.id,
+      text: semester.name,
+      value: semester.id,
+    }));
+    this.setState( {semesterOptions : semesterOptions} );
+  }
+  render() {
+    const { slectedSemester, attendanceData, semesterOptions } = this.state
+    return (
+      <div>
+        <span>Select Semester : </span>
+      <Dropdown placeholder='Select Semester' search selection 
+      options={semesterOptions} onChange={this.getAttendance} value={slectedSemester}>
+      </Dropdown>
+      <Table compact celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Subject </Table.HeaderCell>
+                <Table.HeaderCell>Your Attendance</Table.HeaderCell>
+                <Table.HeaderCell>Total Attendance</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+            {attendanceData.map(attendance => 
+              <Table.Row key={attendance.id}>
+              <Table.Cell>{attendance.subjectName}</Table.Cell>
+              <Table.Cell>{attendance.currentAttendance}</Table.Cell>
+              <Table.Cell>{attendance.totalAttendance}</Table.Cell>
+            </Table.Row>
+            
+            )}
+            </Table.Body>
+          </Table>
+      </div>
+    );
+  }
+
+  getAttendance = (e, { value }) => {
+    this.setState({ slectedSemester : value });
+    let attendance = this.getAttendanceData(value);
+    this.setState({ attendanceData : attendance });
+    console.log(this.state.attendanceData)
+  }
+
+  getAttendanceData = slectedSemester => {
+    debugger;
+    let attendance = data.attendanceList.filter(attendanceObj => attendanceObj.semId == slectedSemester);
+    return attendance;
+  };
 }
+
 
 export default Attendance;
